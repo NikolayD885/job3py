a415728f86ce0db2203b9e501345bb05cfd7c8e4
NikolayD <kolik885@ya.ru>
2022-10-06 21:38:53 +0700

    Программа №5 урок 3

---
+++ 005.py
@@ -0,0 +1,30 @@
+# Задайте число - размер списка. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+# Пример: для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] [Негафибоначчи]
+
+Number = int(input('Введите число: '))
+listA = [0]
+
+
+def Fibonacci(n):
+    if n in [1, 2]:
+        return 1
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+
+
+def NegaFibonacci(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return -1
+    else:
+        num1, num2 = 1, -1
+        for i in range(2, n):
+            num1, num2 = num2, num1 - num2
+        return num2
+
+
+for i in range(1, Number + 1):
+    listA.append(Fibonacci(i))
+    listA.insert(0, NegaFibonacci(i))
+print(listA)
